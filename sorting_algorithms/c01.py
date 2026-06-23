8248a24e96d4c402456c9c1940ab6b222e1e3dd2
Nok Hang Lo <bluetotkc@gmail.com>
2026-06-23 13:40:45 +0100

    feat(sorting algorithms): added exercise 2 (task)

--- sorting_algorithms/c01.py
+++ sorting_algorithms/c01.py
@@ -1,4 +1,5 @@
-DEBUG: bool = True 
+DEBUG: bool = False 
+
 
 def bubble_sort_unoptimised(arr: list[int]) -> list[int]:
     """
@@ -7,6 +8,7 @@ def bubble_sort_unoptimised(arr: list[int]) -> list[int]:
     """
 
     n = len(arr)
+    num_comparisons: int = 0
 
     # outer loop iterates n-1 passes over the array
     # each iteration puts one element into place
@@ -17,12 +19,17 @@ def bubble_sort_unoptimised(arr: list[int]) -> list[int]:
         # inner loop swaps largest element to the end
         for i in range(n - 1):
 
+            num_comparisons += 1
+
             # swap adjacent elements if out of order
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
 
         if DEBUG:
             print(f"Iteration {pass_num}: {arr}")
+
+    if DEBUG:
+        print(f"No. of comparisons = {num_comparisons}")
 
     return arr
 
